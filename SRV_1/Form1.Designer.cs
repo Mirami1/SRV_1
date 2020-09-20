fbe92eefcd8a442077b371751fc07dbeea777cad
Mirami <mishaivanov99@gmail.com>
2020-09-21 02:12:53 +0500

    Добавьте файлы проекта.

---
+++ SRV_1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace SRV_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(213, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 49);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(100, 185);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(232, 49);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(141, 185);
+            this.textBox2.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(39, 281);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(67, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(213, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(298, 281);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Старт_1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 342);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SRV_1/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SRV_1
+{
+    public partial class Form1 : Form
+    {
+        static Queue<string> mes = new Queue<string>();
+        Thread send;
+        Thread get;
+        Thread generator;
+        Semaphore sem;
+        List<Thread> pool;
+        int count;
+        int cc = 0;
+        bool works;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Обычная очередь
+        public void sending()
+        {
+            for(int i = 0; i < numericUpDown1.Value; i++)
+            {
+                string message = (i + 1).ToString();
+                lock (mes)
+                {
+                    mes.Enqueue(message);
+                }
+                BeginInvoke((Action)(() => { textBox1.Text += message + '\r' + '\n'; }));
+                Thread.Sleep(300);
+            }
+           
+        }
+
+        public void getter()
+        {
+            while (true)
+            {
+                if (mes.Count != 0)
+                {
+                    lock (mes)
+                    {
+                        BeginInvoke((Action)(() => { textBox2.Text += mes.Dequeue() + '\r' + '\n'; }));
+                    }
+                }
+                Thread.Sleep(600);
+            }
+        }
+        //Пулл потоков 
+        public void generate()
+        {
+            count = Convert.ToInt32(numericUpDown1.Value);
+            sem = new Semaphore(3, count);
+            for (int i = 0; i < count; i++)
+            {
+                lock (mes)
+                {
+                    mes.Enqueue((i + 1).ToString() + '\r' + '\n');
+                    Invoke((MethodInvoker)(() => textBox1.Text += (i + 1).ToString()));
+                   
+                }
+                Thread.Sleep(200);
+            }
+        }
+
+        public void work(object index)
+        {
+            while (works)
+            {
+                Thread.Sleep(300);
+                sem.WaitOne();
+                
+                lock (mes)
+                {
+                    if (mes.Count!=0)
+                    {
+                        Thread.Sleep(300);
+                        BeginInvoke((Action)(() => { textBox2.Text += mes.Dequeue() + " got by" + index.ToString() + '\r'+'\n'; }));
+                        
+
+                    }
+                    else
+                    {
+                        Thread.Sleep(200);
+                    }
+                 
+                }
+                
+                Thread.Sleep(1000);
+                sem.Release(1);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            send = new Thread(sending);
+            get = new Thread(getter);
+            send.Start();
+            get.Start();
+        }
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            send?.Abort();
+            get?.Abort();
+            works = false;
+            generator?.Abort();
+            foreach (var p in pool ?? new List<Thread>())
+                 p.Abort();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            works= true;
+            generator = new Thread(generate);
+            pool = new List<Thread>();
+            generator.Start();
+            for(int i = 0; i < numericUpDown1.Value; i++)
+            {
+                pool.Add(new Thread(work));
+                pool[i].Start(i);
+            }
+        }
+    }
+}
